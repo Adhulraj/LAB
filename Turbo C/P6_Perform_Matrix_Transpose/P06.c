2663b6aa82f970f3ebbe49f7dec96947d7960bde
Adhulraj K R <adhulrajkr@gmail.com>
2022-07-20 21:36:31 +0530

    Program 06 Matrix Transpose

---
+++ Turbo C/P6_Perform_Matrix_Transpose/P06.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+    int a[10][10],transpose[10][10],r,c,i,j;
+    printf("enter number of Rows and Columns : ");
+    scanf("%d %d",&r,&c);
+
+    //Assigning elements to a matrix
+    printf("Enter the elements of the matrix a[i][j]");
+    
+}
