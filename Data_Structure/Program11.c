e7dcf629d2eca64db0af1cf2bd6e66b24ed6aaf5
Adhulraj K R <adhulrajkr@gmail.com>
2022-09-28 20:51:46 +0530

    Update

--- Data_Structure/Program11.c
+++ Data_Structure/Program11.c
@@ -25,57 +25,57 @@ void main()
         switch(choice)
         {
             case 1:
-            printf("Enter the above: ");
-            scanf("%d",&item);
-            x=(struct node*)malloc(sizeof(struct node));
-            x->data=item;
-            x->link=NULL;
-            p->link=x;
-            p=x;
-            top=x;
-            break;
+                printf("Enter the above: ");
+                scanf("%d",&item);
+                x=(struct node*)malloc(sizeof(struct node));
+                x->data=item;
+                x->link=NULL;
+                p->link=x;
+                p=x;
+                top=x;
+                break;
             
             case 2:
-            if(top->data==-1)
-            {
-                printf("Stack is empty!");
-            }
-            else
-            {
-                p=head;
-                while(p->link!=top)
+                if(top->data==-1)
                 {
-                    p=p->link;
+                    printf("Stack is empty!");
                 }
-                ditem=top->data;
-                top=p;
-                top->link=NULL;
-                free(p->link);
-                printf("Popped item is: %d", ditem);
-            }
-            break;
+                else
+                {
+                    p=head;
+                    while(p->link!=top)
+                    {
+                        p=p->link;
+                    }
+                    ditem=top->data;
+                    top=p;
+                    top->link=NULL;
+                    free(p->link);
+                    printf("Popped item is: %d", ditem);
+                }
+                break;
             
             case 3:
-            if(top->data==-1)
-            {
-                printf("\n Stack is Empty!");
-            }
-            else
-            {
-                printf("Stack Listed is: ");
-                p=head;
-                p=p->link;
-                while(p->link!=NULL)
+                if(top->data==-1)
                 {
-                    printf("%d", p->data);
-                    p=p->link;
+                    printf("\n Stack is Empty!");
                 }
-                printf("%d", p->data);
-            }
-            break;
+                else
+                {
+                    printf("Stack Listed is: ");
+                    p=head;
+                    p=p->link;
+                    while(p->link!=NULL)
+                    {
+                        printf("%d", p->data);
+                        p=p->link;
+                    }
+                    printf("%d", p->data);
+                }
+                break;
             
             case 4:
-            printf("\n Exit...");
+                printf("\n Exit...");
         }
     }
             while(choice<4);
