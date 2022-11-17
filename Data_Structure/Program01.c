408f007db0289e8604a9bcd7c89d2d588e15d00f
Adhulraj K R <adhulrajkr@gmail.com>
2022-11-17 17:52:14 +0530

    Reverse of String Using Pointers

--- Data_Structure/Program01.c
+++ Data_Structure/Program01.c
@@ -1,5 +1,30 @@
+/*
+		REVERSE OF A STRING USING POINTERS
+	-----------------------------------------
+
+	AIM :
+		Program to reverse a string using pointers
+
+	ALGORITHM :
+
+		STEP 01: START
+		STEP 02: Declare char str,rev,*strptr,*revptr
+		STEP 03: Read str
+		STEP 04: Repeat Step 5 while *strtr
+		STEP 05: 	Increment strptr,i
+		STEP 06: Repeat Step 7 to Step 9 while i>=0
+		STEP 07:		Decrement strptr,i
+		STEP 08: 	*revptr=*strptr
+		STEP 09: 	increment *revptr
+		STEP 10: STOP
+
+
+*/
+
+
 #include<stdio.h>
 #include<conio.h>
+
 void main()
 {
 	int i=-1;
@@ -8,7 +33,7 @@ void main()
 	char *revptr = rev;
 	printf("Enter the string : ");
 	scanf("%s",&str);
-	while(*strptr){
+	while(*strptr){ //get length of str
 		strptr++;
 		i++;
 	}
@@ -21,3 +46,13 @@ void main()
 	printf("Reverse of the string is : %s",rev);
 	getch();
 }
+
+/*
+	---------------------------------------------------
+
+	OUTPUT
+
+	Enter the string : hello
+	Reverse of the string is : olleh
+
+*/
