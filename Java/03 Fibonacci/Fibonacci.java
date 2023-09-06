e6e454ea08ca2c8aeddc14608fe3483379f3dbc0
Adhulraj K R <adhulrajkr@gmail.com>
2023-09-06 12:01:16 +0530

    Java 03

--- Java/03 Fibonacci/Fibonacci.java
+++ Java/03 Fibonacci/Fibonacci.java
@@ -1,3 +1,18 @@
+/*
+ AIM
+ To write a program to display Fibonacci series upto a limit
+
+ ALGORITHM:
+	Step 01: START
+	Step 02: Declare n,a=0,b=1,c=1
+	Step 03: Read n
+	Step 04: print 0
+	Step 05: while n>1 loop Step 06 and Step 07
+	Step 06: Print c
+	Step 07: Set a=b,b=c,n=n-1
+	Step 08: STOP
+ */
+
 import java.util.Scanner;
 
 class Fibonacci {
@@ -17,3 +32,10 @@ class Fibonacci {
         }
     }
 }
+
+/*
+ * OUTPUT
+ * Enter the limit:
+*  10
+    Fibonacci Series upto 10 : 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+ */
