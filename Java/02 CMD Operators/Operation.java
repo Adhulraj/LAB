bf84b09715d024824c53c3f5b6bee7232a4b9f78
Adhulraj K R <adhulrajkr@gmail.com>
2023-09-06 11:30:35 +0530

    Java 02

--- Java/02 CMD Operators/Operation.java
+++ Java/02 CMD Operators/Operation.java
@@ -7,6 +7,29 @@
  * 	Step 01: START
  * 	Step 02:
  */
+import java.io.*;
+import java.util.*;
+
 public class Operation {
-    
+    public static void main(String[] args) throws IOException{
+		int a,b,sum,diff,mul,quo,rem;
+		a = Integer.parseInt(args[0]);
+		b = Integer.parseInt(args[1]);
+		sum= a+b;
+		diff = a-b;
+		mul = a*b;
+		quo = a/b;
+		rem = a%b;
+		System.out.println("The sum = "+sum);
+		System.out.println("The diff = "+diff);
+		System.out.println("The product = "+mul);
+		System.out.println("The quo = "+quo);;
+		System.out.println("The rem = "+rem);
+	}
 }
+
+/*
+ * OUTPUT
+ * 
+ * 
+ */
