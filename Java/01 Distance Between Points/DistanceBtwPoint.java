39a73d46f1cce8f142dc5d29e117904a973d260d
Adhulraj K R <adhulrajkr@gmail.com>
2023-09-06 11:03:14 +0530

    Update

--- Java/01 Distance Between Points/DistanceBtwPoint.java
+++ Java/01 Distance Between Points/DistanceBtwPoint.java
@@ -1,3 +1,16 @@
+/* 
+AIM :
+    Write a java program to claculate distance between two points
+
+ ALGORITHM:
+    Step 1:START
+    Step 2:Declare x1,x2,y1,y2,dis
+    Step 3:Read points x1,y1,x2,y2
+    Step 4:Calculate d = ✓(x2-x1)²=(y2-y1)²
+    Step 5:Print d
+    Step 6:STOP
+ */
+
 import java.util.Scanner;
 class DistanceBtwPoint{
     public static void main(String[] args) {
@@ -18,3 +31,19 @@ class DistanceBtwPoint{
 
     }
 }
+
+
+/*
+
+OUTPUT
+
+Enter x1 point: 
+2
+Enter x2 point:
+4
+Enter y1 point:
+6
+Enter y2 point:
+8
+Distance between (2, 6) and (4, 8) is 2.8284271247461903
+ */
