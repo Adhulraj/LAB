aed9b5dd387eac60f705269d596e723b06aa3640
Adhulraj K R <adhulrajkr@gmail.com>
2023-09-06 11:16:37 +0530

    Update

--- Java/02 CMD Operators/Operation.java
+++ Java/02 CMD Operators/Operation.java
@@ -1,4 +1,12 @@
-
+/*
+ * AIM:
+ * Write a program to find sum, differnce, product, quotient an dremainder of two numbers passed as command line
+ * 
+ * 
+ * ALGORITHM:
+ * 	Step 01: START
+ * 	Step 02:
+ */
 public class Operation {
     
 }
