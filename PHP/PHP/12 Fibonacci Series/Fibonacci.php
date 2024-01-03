0ee872533cb1273b312bc5962bc0c264b79c9342
Adhulraj K R <adhulrajkr@gmail.com>
2024-01-03 12:09:47 +0530

    PHP 12

--- PHP/PHP/12 Fibonacci Series/Fibonacci.php
+++ PHP/PHP/12 Fibonacci Series/Fibonacci.php
@@ -4,5 +4,30 @@ AIM
     To display the Fibonacci series up to a given number.
 
  -->
+<html>
+    <head>
+        <title>Fibonacci Series</title>
+    </head>
+    <body>
+        <h2>Fibonacci Series</h2>
+        <form action="" method="post">
+            <input type="number" name="num" >
+            <input type="submit" value="Submit" name="submit_btn">
+        </form>
+    </body>
+</html>
+<?php
+    if(isset($_POST['num'])){
+        $n = $_POST['num'];
+        echo "Fibonacci Numbers ";
+        $f = 0; $s=1;
+        echo $f." ".$s;
+        for ($i=2; $i < $n; $i++) { 
+            $t = $f +$s;
+            echo " ".$t;
+            $f = $s;
+            $s = $t;
+        }
+    }
+?>
 
- 
