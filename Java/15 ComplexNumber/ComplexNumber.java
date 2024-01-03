8df3c0afb9cc76e47a1dc5daa495d7a4d728af08
athulkrishnama <athulkrishnama24@gmail.com>
2024-01-03 22:21:18 +0530

    java program 12 - 16 algorithm added

--- Java/15 ComplexNumber/ComplexNumber.java
+++ Java/15 ComplexNumber/ComplexNumber.java
@@ -1,5 +1,24 @@
 //TODO OUTPUT
 
+// AIM
+
+// Using class and objects, write a java program to find the sum of two complex
+// numbers (Hint: Use object as parameter to function).
+
+
+// Algorithm
+// Step 01 : Start
+// Step 02 : Declare Complex numbers 'a' & 'b'
+// Step 03 : Read real part of 'a' as r1
+//    & imaginary part as img1
+// Step 04 : Read real part of 'b' as r2
+//    & imaginary part as img2
+// Step 05 : Declare Complex number 'c'
+// Step 06 : Assign real part of 'c' as r1 + r2
+//    & imaginary part as img1 + img2
+// Step 07 : Print real & imaginary part of Complex number 'c'
+// Step 08 : Stop
+
 public class ComplexNumber {
     int real, imaginary;
     ComplexNumber(int real, int imaginary){
