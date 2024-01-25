1b961bdf27524a5c3a876d53c552cf5f4686a19e
Adhulraj K R <adhulrajkr@gmail.com>
2024-01-25 11:50:46 +0530

    Linux 13

---
+++ Linux/13 Factorial Of A Number/factorial.sh
@@ -0,0 +1,25 @@
+#AIM
+#   Shell script find the factorial of a given number
+
+#ALGORITHM
+
+# Step 01: START
+# Step 02: The script asks the user for a number.
+# Step 03: It sets a result to 1
+# Step 04: Counts down from the given number to 1:
+#    - Multiplies the result by the current number
+# Step 05: Show the Factorial
+# Step 06: STOP
+
+
+
+read -p "Enter the number: " num
+fact=1
+for (( i=$num;i>=1;i--)); do    
+    fact=$(( $fact*i))
+done
+echo "The factorial is $fact"
+
+#OUTPUT
+#Enter the number: 5
+# The factorial is 120
